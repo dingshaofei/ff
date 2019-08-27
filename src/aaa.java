0472909e7f1c3b010a87dc45b7cda8f7f4662ef7
dsf-java <525077624@qq.com>
2019-08-27 20:28:27 +0800

    Initial commit

---
+++ src/aaa.java
@@ -0,0 +1,14 @@
+public class aaa {
+    private int uid;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public static void main(String[] args) {
+
+    }
+}
